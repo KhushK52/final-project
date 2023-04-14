3740b321abe5745f365cf5d62926d2ccb01c306a
Octavio L <octavio.lomelicastro@gmail.com>
2023-04-14 12:53:56 -0700

    reddit route works

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const NewsAPI = require('newsapi');
 const axios = require("axios");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 const newsapi = new NewsAPI("443d03c9e90443cdad2471c1896ecf96");
 const app = express();
 
@@ -15,6 +16,7 @@ InitiateMongoServer();
 
 app.use(express.static('public'))
 app.use(express.json());
+app.use(bodyParser.json());
 
 
 // Routes
@@ -26,26 +28,39 @@ app.get('/', (req, res) => {
     res.render(__dirname + 'index.html');
 })
 
-// Find all tasks
+
+/*
+    @description: sends all tasks in the database as a response
+    @status: works correctly
+*/
 app.get('/tasks', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
 })
 
-// Find all tasks with certain tag
+/*
+    @description: sends all tasks in the database with matching tag as a response
+    @status: works correctly
+*/
 app.get('/tasks/:tag', async (req, res) => {
     const tasks = await Task.find({tag: req.params.tag})
     res.json(tasks);
 })
 
-// Create task with specified title and tag and save to database
+/*
+    @description: creates tasks with specified title and tag
+    @status: works correctly
+*/
 app.post('/createTask', async (req, res) => {
     const newTask = new Task({title: req.query.title, tag: req.query.tag});
     await newTask.save();
     res.json(newTask);
 })
 
-// Delete the task with the specified id
+/*
+    @description: deletes task from database with matching id
+    @status: works correctly
+*/
 app.post('/deleteTask/:id', async (req, res) => {
     const foundTask = await Task.deleteOne({id : req.params.id});
     res.send(foundTask);
@@ -53,8 +68,9 @@ app.post('/deleteTask/:id', async (req, res) => {
 
 
 /*
-@description: Returns data about news in berkeley
-@return: JSON object
+    @description: Returns data about news in berkeley
+    @return: JSON object
+    @status: not tested yet
 */
 app.get('/news', (req, res) => {
     newsapi.v2.topHeadlines({
@@ -68,11 +84,12 @@ app.get('/news', (req, res) => {
 
 
 /*
-@description: Returns data about the day's weather in Berkeley by using the Weather API
-@return: JSON object
-    temp: temperature in Fahrenheit
-    status: description of the weather
-    date: the hour for the data
+    @description: Returns data about the day's weather in Berkeley by using the Weather API
+    @return: JSON object
+        temp: temperature in Fahrenheit
+        status: description of the weather
+        date: the hour for the data
+    @status: not tested yet
 */
 app.get('/weather', (req, res) => {
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${37.867876}&lon=${-122.255469}&appid=3453ef5a617b0f8aff2cd4426a958d7f&units=imperial`
@@ -91,29 +108,31 @@ app.get('/weather', (req, res) => {
 
 
 /*
-@description: Returns data about 3 "hot" posts on Reddit's Berkeley subreddit
-@return: JSON object
-    time_ago: number of hours ago the post was created
-    upvotes: number of upvotes the post received
-    flair: the flair attached to the post
-    title: the title of the post
-    url: a link to the post
+    @description: Returns data about 3 "hot" posts on Reddit's Berkeley subreddit
+    @return: JSON object
+        time_ago: number of hours ago the post was created
+        upvotes: number of upvotes the post received
+        flair: the flair attached to the post
+        title: the title of the post
+        url: a link to the post
+    @status: works correctly
 */
 app.get('/reddit', (req, res) => {
-    axios.get("https://www.reddit.com/r/berkeley/hot/.json")
+    axios.get("https://www.reddit.com/r/berkeley/hot/.json", {headers: {"Content-Type": "application/json; charset=UTF-8"}})
     .then(response => {
         let data = {time_ago: [], upvotes: [], flair: [], title: [], url: []};
         for (var i = 2; i < 5; i++) {
-            let calculated_time = new Date(response.data.children[i].data.created_utc * 1000);
+            
+            let calculated_time = new Date(response.data.data.children[i].data.created_utc * 1000);
             calculated_time = Math.round((new Date() - calculated_time) / 3600000);
             data.time_ago.push(calculated_time);
-            data.upvotes.push(response.data.children[i].data.ups);
-            data.flair.push(response.data.children[i].data.link_flair_text);
-            data.title.push(response.data.children[i].data.title);
-            data.url.push(result.data.children[i].data.permalink);
+            data.upvotes.push(response.data.data.children[i].data.ups);
+            data.flair.push(response.data.data.children[i].data.link_flair_text);
+            data.title.push(response.data.data.children[i].data.title);
+            data.url.push(response.data.data.children[i].data.permalink);
         }
         res.send(data);
-    })
+     })
     .catch(error => console.log(error)) 
 })
 
